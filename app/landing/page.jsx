07662e2cb3bbe2b5d4e02b18d60dd77f86d4fd88
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2024-12-25 03:16:00 -0500

    smallrefactor1

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -6,8 +6,8 @@ import MainSection from "./main-section/main";
 export default function HeroSection() {
   return (
     <>
-      <Navbar className="absolute top-0 left-0 right-0 z-10" />
       <div className="relative min-h-screen">
+        <Navbar className="absolute top-0 left-0 right-0 z-10" />
         <MainSection />
       </div>
     </>
