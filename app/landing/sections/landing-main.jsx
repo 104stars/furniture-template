f033eccda757c243798c960e73d239d627ff83a9
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2024-12-26 21:00:19 -0500

    fixed responsiveness

--- app/landing/sections/landing-main.jsx
+++ app/landing/sections/landing-main.jsx
@@ -3,7 +3,7 @@ import CarouselComponent from "@/components/carousel/carousel";
 
 export default function MainSection() {
   return (
-    <main className="flex flex-col  lg:flex-row items-center justify-center gap-6 sm:gap-8 lg:gap-16 py-20 sm:py-20 lg:py-20 xl:py-40 px-4 sm:px-6 lg:px-8">
+    <main className="flex flex-col min-h-screen lg:flex-row items-center justify-center gap-6 sm:gap-8 lg:gap-16 py-20 sm:py-20 lg:py-20 xl:py-40 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-2xl lg:max-w-3xl">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-inter font-bold text-left">
           Transform your home into a sanctuary
